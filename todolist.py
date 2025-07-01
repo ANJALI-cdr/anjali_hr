55dc9420b13f4ffa0bfc82ecffe63a4ccac143b3
anjali_hr <anjalihr03@gmail.com>
2025-07-01 13:42:45 +0530

    change the output appearance

--- todolist.py
+++ todolist.py
@@ -5,9 +5,16 @@ for key,value in L.items():
 while True:
    a=input('any other tasks (yes/no)?')
    if a=='yes':
-      L['dance']='under progress'
-   else:
+      task=input(" enter the task:")
+      b=input("enter the status:")
+      L[task]=b
+   elif a=='no':
       print("Thats fine")
       break
-print("The status of task are:",L)
+   else:
+      print('invalid input')
+      continue
+for key, value in L.items():
+   print(f"{key} : {value}")
+#print("The status of task are:",L)
 
