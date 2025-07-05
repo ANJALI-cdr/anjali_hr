f7b9b3c7307278d80d98308121b3c10f74d23dd3
anjali_hr <anjalihr03@gmail.com>
2025-07-05 12:36:27 +0530

    finalised the code

--- pythonproject1.py
+++ pythonproject1.py
@@ -1,43 +1,35 @@
 count=0
-#quesion1
-print("1) Which company developed the Windows operating system?")
-o={'a':'Apple','b':'Google','c':'Microsoft','d':'IBM'}
-for key,value in o.items():
-        print (f"{key}) {value}")
-user_choice = input("enter the options a/b/c/d:")
-if o[user_choice]=='Microsoft':
+def quiz(question,option,solution):
+   global count
+   print(question)
+   for key , value in option.items():
+      print (f"{key}) {value}") 
+   user_option=input("enter the options a/b/c/d:").lower()
+   if user_option==solution:
          count+=1
-#question 2
-print("2) Which of the following is an operating system?")
-o={'a':'Google','b':'Linux','c':'Intel','d':'Firefox'}
-for key,value in o.items():
-        print (f"{key}) {value}")
-user_choice = input("enter the options a/b/c/d:")
-if o[user_choice]=='Linux':
-         count+=1
-#question3
-print("3)What does HTTP stand for?")
-o={'a':'HyperText Transfer Protocol','b':'HighText Transfer Protocol',
-       'c':'HyperText Transmission Protocol','d':'High Transfer Text Protocol'}
-for key,value in o.items():
-        print (f"{key}) {value}")
-user_choice = input("enter the options a/b/c/d:")
-if o[user_choice]=='HyperText Transfer Protocol':
-         count+=1
-#question4
-print("4) Which of the following is used to store data permanently?")
-o={'a':'RAM','b':'ROM','c':'CPU','d':'Cache'}
-for key,value in o.items():
-        print (f"{key}) {value}")
-user_choice = input("enter the options a/b/c/d:")
-if o[user_choice]=='ROM':
-         count+=1
-#question5
-print("5) What does IP stand for in networking?")
-o={'a':'Internal Protocol','b':'Internet Process','c':'Internet Protocol','d':' Input Program'}
-for key,value in o.items():
-        print (f"{key}) {value}")
-user_choice = input("enter the options a/b/c/d:")
-if o[user_choice]=='Internet Protocol':
-         count+=1
-print("total score is:",count)
+         print("wow!correct answer.")
+   else:
+         print("wrong, the correct option is:",solution)
+quiz("1) Which company developed the Windows operating system?",{'a':'Apple','b':'Google','c':'Microsoft',
+        'd':'IBM'},'c')
+quiz("2) Which of the following is an operating system?",{'a':'Google','b':'Linux','c':'Intel',
+         'd':'Firefox'},'b')
+quiz("3)What does HTTP stand for?",{'a':'HyperText Transfer Protocol','b':'HighText Transfer Protocol',
+       'c':'HyperText Transmission Protocol','d':'High Transfer Text Protocol'},'b')
+quiz("4) Which of the following is used to store data permanently?",{'a':'RAM','b':'ROM','c':'CPU',
+      'd':'Cache'},'b')
+quiz("5) What does IP stand for in networking?",{'a':'Internal Protocol','b':'Internet Process',
+      'c':'Internet Protocol','d':' Input Program'},'b')
+print("The score is:",count)
+percentage = (count / 5) * 100
+print("your percentage:",percentage)
+if percentage == 100:
+    print("Excellent! You got all questions right.")
+elif percentage >= 60:
+    print("Good job! You passed.")
+else:
+    print("Keep practicing. You can do better!")
+        
+
+
+
